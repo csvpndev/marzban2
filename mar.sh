738a8f2c47c3aabad739031b515e7f7783211581
csvpndev <128266371+csvpndev@users.noreply.github.com>
2024-11-28 05:48:21 +0700

    Update mar.sh

--- mar.sh
+++ mar.sh
@@ -235,7 +235,7 @@ apt install socat cron bash-completion -y
 
 #install cert
 curl https://get.acme.sh | sh -s email=$email
-/root/.acme.sh/acme.sh --server letsencrypt --register-account -m $email --issue --alpn -d $domain --standalone -k ec-256 --force --debug
+/root/.acme.sh/acme.sh --server letsencrypt --register-account -m $email --issue -d $domain --standalone -k ec-256 --debug
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /var/lib/marzban/xray.crt --keypath /var/lib/marzban/xray.key --ecc
 wget -O /var/lib/marzban/xray_config.json "https://raw.githubusercontent.com/csvpndev/marzban2/main/xray_config.json"
 
